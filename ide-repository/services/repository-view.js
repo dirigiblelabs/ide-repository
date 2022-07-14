c631a868a73c68a3e23fa6382d4d09424fbe79bf
StanZGenchev <stanslav.genchev@sap.com>
2022-07-14 22:15:07 +0300

    Perspective and menu fixes

--- ide-repository/services/repository-view.js
+++ ide-repository/services/repository-view.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2010-2022 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -8,13 +8,15 @@
  * Contributors:
  *   SAP - initial API and implementation
  */
-exports.getView = function () {
-	return {
-		"id": "repository",
-		"name": "Repository",
-		"factory": "frame",
-		"region": "left-top",
-		"label": "Repository",
-		"link": "../ide-repository/repository.html"
-	};
+const viewData = {
+	id: "repository",
+	label: "Repository",
+	factory: "frame",
+	region: "left",
+	link: "../ide-repository/repository.html"
+};
+if (typeof exports !== 'undefined') {
+	exports.getView = function () {
+		return viewData;
+	}
 }
--- ide-repository/services/snapshot-view.js
+++ ide-repository/services/snapshot-view.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2010-2022 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -8,13 +8,15 @@
  * Contributors:
  *   SAP - initial API and implementation
  */
-exports.getView = function () {
-	return {
-		"id": "snapshot",
-		"name": "Snapshot",
-		"factory": "frame",
-		"region": "left-top",
-		"label": "Snapshot",
-		"link": "../ide-repository/snapshot.html"
-	};
+const viewData = {
+	id: "snapshot",
+	label: "Snapshot",
+	factory: "frame",
+	region: "left",
+	link: "../ide-repository/snapshot.html"
+};
+if (typeof exports !== 'undefined') {
+	exports.getView = function () {
+		return viewData;
+	}
 }
